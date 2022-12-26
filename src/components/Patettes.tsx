6edd628a2ea80a9429ec8b65e8cc254547e5e3c3
Yukiniro <yukiniro@hotmail.com>
2022-12-26 23:07:17 +0800

    feat: support color transform

--- src/components/Patettes.tsx
+++ src/components/Patettes.tsx
@@ -1,3 +1,7 @@
+import Color from "color";
+import { useMemo } from "react";
+import { fixed } from "bittydash";
+
 type PalettesProps = {
   hexs: string[];
 };
@@ -6,12 +10,46 @@ type PalettesItemProps = {
   hex: string;
 };
 
+type TagProps = {
+  title: string;
+  text: string;
+};
+
+function Tag(props: TagProps) {
+  const { title, text } = props;
+  return (
+    <div className="text-left mt-2 mb-2">
+      <p className="text-size-4 uppercase">{title}</p>
+      <p className="text-size-2">{text}</p>
+    </div>
+  );
+}
+
 function PalettesItem(props: PalettesItemProps) {
   const { hex } = props;
+
+  const color = useMemo(() => {
+    return Color(hex);
+  }, [hex]);
+
+  const { rgb, hsl, cmyk } = useMemo(() => {
+    const joinArr = (arr: number[]): string => {
+      return arr.map(value => fixed(value, 1)).join(",");
+    };
+    return {
+      rgb: joinArr(color.rgb().array()),
+      hsl: joinArr(color.hsl().array()),
+      cmyk: joinArr(color.cmyk().array()),
+    };
+  }, [color]);
+
   return (
     <div className="flex flex-col items-center">
       <div className="w-16 h-16 m-b-4" style={{ backgroundColor: hex }}></div>
-      <p>{hex}</p>
+      <Tag title="hex" text={hex} />
+      <Tag title="rgb" text={rgb} />
+      <Tag title="hsl" text={hsl} />
+      <Tag title="cmyk" text={cmyk} />
     </div>
   );
 }
